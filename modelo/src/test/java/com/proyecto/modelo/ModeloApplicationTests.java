b52025d43ec534bdb21fe0ed7fdf713c18ac3c4b
jmrm92 <jmrm81992@gmail.com>
2023-10-06 15:14:17 +0200

    test

--- modelo/src/test/java/com/proyecto/modelo/ModeloApplicationTests.java
+++ modelo/src/test/java/com/proyecto/modelo/ModeloApplicationTests.java
@@ -1,10 +1,44 @@
 package com.proyecto.modelo;
 
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.proyecto.modelo.dao.ModeloDao;
+import com.proyecto.modelo.model.Modelo;
+import com.proyecto.modelo.service.ModeloServiceImpl;
 
 @SpringBootTest
 class ModeloApplicationTests {
 
-	
+    @Mock
+    private ModeloDao dao;
+
+    @InjectMocks
+    private ModeloServiceImpl service;
+
+    @Test
+    public void testActualizarPiezasNecesarias() {
+        // Paso 1
+        Modelo modelo = new Modelo();
+        modelo.setPiezasNecesarias(10);
+
+        when(dao.findById(1)).thenReturn(Optional.of(modelo));
+
+        service.actualizarPiezasNecesarias(1, 20);
+
+        verify(dao).findById(1);
+        verify(dao).save(argThat(updatedModelo -> updatedModelo.getPiezasNecesarias() == 20));
+
+        verifyNoMoreInteractions(dao);
+    }
 
 }
